d522daad40be7b9566e3c772ef2d1eb773656e2d
♠JPex♠ <79031419+JBlades10@users.noreply.github.com>
2021-09-02 19:16:19 -0400

    Add files via upload
    Linked list that insert and print values.

---
+++ linkedListAuto.py
@@ -0,0 +1,40 @@
+#creates a linked list manuelly
+class linkedListNode:
+    def __init__(self, value, nextNode=None):
+    	self.value = value
+    	self.nextNode = nextNode
+#creates a linked list from the head Node
+#	keeps track of the head
+class linkedList:
+	def __init__(self, head=None):
+		self.head = head
+
+	def insert(self, value):
+		node = linkedListNode(value)
+		if self.head is None:
+			self.head = node
+			return
+
+		currentNode = self.head
+		while True:
+			if currentNode.nextNode is None:
+				currentNode.nextNode = node
+				break
+			currentNode =currentNode.nextNode
+
+	def printLinkedList(self):
+		currentNode = self.head
+		while currentNode is not None:
+			print (currentNode.value, "->", end=" ")
+			currentNode = currentNode.nextNode
+		print("None")
+
+
+ll = linkedList()
+ll.printLinkedList()
+ll.insert("3")
+ll.printLinkedList()
+ll.insert("44")
+ll.printLinkedList()
+ll.insert("55")
+ll.printLinkedList()
